cd1ea0602f6d4aaef3e028e8432950917c4cef51
Thomas Woinke <thomas@steckschwein.de>
2025-01-19 15:56:34 +0100

    exports

--- steckos/kernel/jumptable.s
+++ steckos/kernel/jumptable.s
@@ -1,9 +1,12 @@
 .segment "JUMPTABLE"    ; "kernel" jumptable
 .importzp out_vector, in_vector
-.import primm, set_input, set_output
+.import primm, set_input, set_output, xmodem_upload
 .import upload
+.export krn_chrin, krn_chrout, krn_primm, krn_set_input, krn_set_output
 krn_chrin:        jmp (out_vector)
 krn_chrout:       jmp (in_vector)
 krn_primm:        jmp primm
 krn_set_output:   jmp set_output
 krn_set_input:    jmp set_input
+krn_upload:       jmp xmodem_upload
+
