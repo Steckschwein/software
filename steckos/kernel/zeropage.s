a98ed288129ad9cea1624f3c8eb40f6abdd09c35
Thomas Woinke <thomas@steckschwein.de>
2025-04-05 20:27:01 +0200

    zp reorg

--- steckos/kernel/zeropage.s
+++ steckos/kernel/zeropage.s
@@ -1,59 +1,120 @@
+.exportzp out_vector, in_vector
+.exportzp startaddr
+.exportzp console_ptr, cursor_ptr
+.exportzp scroll_src_ptr, scroll_trg_ptr 
+.exportzp vdp_ptr
+.exportzp spi_sr 
+.exportzp retvec 
+.exportzp paramptr
+.exportzp cmdptr
+.exportzp bufptr
+.exportzp pathptr
+.exportzp dumpvecs
+.exportzp dirptr
+.exportzp filenameptr
+.exportzp sd_blkptr
+.exportzp tmp_ptr
+.exportzp volatile_tmp
+.exportzp volatile_tmp2
+
+.exportzp XAML;  Last "opened" location Low
+.exportzp XAMH;  Last "opened" location High
+.exportzp STL ;  Store address Low
+.exportzp STH ;  Store address High
+.exportzp L   ;  Hex value parsing Low
+.exportzp H   ;  Hex value parsing High
+.exportzp YSAV ;  Used to see if hex value is given
+.exportzp MODE ;  $00=XAM, $7F=STOR, $AE=BLOCK XAM
+
+.exportzp retryl
+.exportzp retryh
+.exportzp crc
+.exportzp crch
+.exportzp blkno
+.exportzp protocol
+.exportzp block_rx_cb
+
+
+.zeropage
 ; vectors for input/output routines
-.exportzp out_vector        = $d0
-.exportzp in_vector         = $d2
+out_vector:       .res 2
+in_vector:        .res 2
 
 ; startaddress for uploaded images
-.exportzp startaddr         = $d4
+startaddr:         .res 2
 
 ; console stuff
 ; pointer to current screen buffer
-.exportzp console_ptr       = $d6
+console_ptr:      .res 2
+
 ; pointer to cursor position within screen buffer
-.exportzp cursor_ptr        = $d8
+cursor_ptr:       .res 2
+
 ; scroll source pointer
-.exportzp scroll_src_ptr    = $da 
+scroll_src_ptr:   .res 2
 ; scroll target pointer
-.exportzp scroll_trg_ptr    = $dc 
+scroll_trg_ptr:   .res 2
+
 ; vdp pointer 
-.exportzp vdp_ptr           = $de
+vdp_ptr:          .res 2
 
 ; SPI shift register
-.exportzp spi_sr            = $e0
+spi_sr:           .res 2
+
+; shell return vector
+retvec:           .res 2
+
+; shell parameter pointer
+paramptr:         .res 2
+
+; shell command pointer
+cmdptr:           .res 2
+
+; shell buffer pointer
+bufptr:           .res 2
+
+; temp
+volatile_tmp:     .res 1
+volatile_tmp2:    .res 1
+
+; path ptr
+pathptr:          .res 2
+
+; shell dump command vectors
+dumpvecs:         .res 4
+
+; directory pointer
+dirptr:           .res 2
+
+; filename pointer
+filenameptr:      .res 2
+
+; sd card block pointer
+sd_blkptr:        .res 2
+
+; temp volatile pointer for general usage
+tmp_ptr:          .res 2
 
 ; stuff for wozmon
-.exportzp XAML              = $e1  ;  Last "opened" location Low
-.exportzp XAMH              = $e2  ;  Last "opened" location High
-.exportzp STL               = $e3  ;  Store address Low
-.exportzp STH               = $e4  ;  Store address High
-.exportzp L                 = $e5  ;  Hex value parsing Low
-.exportzp H                 = $e6  ;  Hex value parsing High
-.exportzp YSAV              = $e7  ;  Used to see if hex value is given
-.exportzp MODE              = $e8  ;  $00=XAM, $7F=STOR, $AE=BLOCK XAM
+XAML=             startaddr
+XAMH=             startaddr+1
+STL =             dumpvecs
+STH =             dumpvecs+1
+L   =             dumpvecs+2
+H   =             dumpvecs+3
+YSAV=             volatile_tmp
+MODE=             volatile_tmp2
 
-.exportzp retvec            = $e1
-.exportzp paramptr          = $e3
-.exportzp cmdptr            = $e5 
-; .exportzp msg_ptr           = $e7
-.exportzp volatile_tmp      = $e7
 
-.exportzp bufptr            = $e9
 
 ; xmodem upload
-.exportzp blkno             = $e9  ; block number
-.exportzp retryl            = $ea  ; 16 bit retry
-.exportzp retryh            = $eb  ;
-.exportzp protocol          = $ec  ; 2nd counter
-.exportzp block_rx_cb       = $ed  ; callback
-.exportzp crc               = $ef  ; CRC lo byte  (two byte variable)
-.exportzp crch              = $f0  ; CRC hi byte
-
-.exportzp pathptr           = $f1 
-.exportzp dumpvecs          = $f3
-.exportzp dirptr            = $f7
-.exportzp filenameptr       = $f9
-.exportzp volatile_tmp2     = $fb
+retryl=           dumpvecs    ; 16 bit retry
+retryh=           dumpvecs+1
+crc=              dumpvecs+2  ; CRC lo byte  (two byte variable)
+crch=             dumpvecs+3  ; CRC hi byte
+blkno=            volatile_tmp ; block number
+protocol=         volatile_tmp2 ; 2nd counter
+block_rx_cb=      tmp_ptr
 
 
-.exportzp sd_blkptr         = $fc  ; sd card block pointer
-; temp volatile pointer for general usage
-.exportzp tmp_ptr           = $fe
+
