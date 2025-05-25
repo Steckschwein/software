fa09e45468c29eb7b6f01e15a0ba1c14030aa2dc
Thomas Woinke <thomas@steckschwein.de>
2025-05-25 15:26:33 +0200

    progs

--- mkimg.sh
+++ mkimg.sh
@@ -12,7 +12,7 @@ fi
 truncate -s $size ${img}
 mkfs -t fat -F 32 -s 2  ${img} -n ${version^^}
 
-mmd -i ${img} ::/steckos # ::/demo ::/games ::/progs ::/basic ::/basic/benchmrk
+mmd -i ${img} ::/steckos ../progs # ::/demo ::/games ::/progs ::/basic ::/basic/benchmrk
 
 mcopy -i ${img}  steckos/tools/*.prg $TARGET/steckos/
 # mcopy -i ${img}  steckos/demo/*.prg $TARGET/demo/
@@ -23,12 +23,12 @@ mcopy -i ${img}  steckos/tools/*.prg $TARGET/steckos/
 # GAMES="games/dinosaur/dinosaur.prg games/pong/pong.prg games/microchess/mchess.prg games/shmup/shmup.prg games/pacman/pacman.prg"
 # mcopy -i ${img} $GAMES $TARGET/games/
 
-# PROGS=$(find progs -name "*.prg")
-# mcopy -i ${img} $PROGS $TARGET/progs/
+#PROGS=$(find progs -name "*.prg")
+#mcopy -i ${img} $PROGS $TARGET/progs/
 
 # mcopy -i ${img} progs/ehbasic_65c02/demo/* $TARGET/basic
 # mcopy -i ${img} progs/ehbasic_65c02/benchmark/* $TARGET/basic/benchmrk
 
-# if [ -d "../local" ] ; then
-# 	mcopy -snvom -i ${img} ../local/* $TARGET/
-# fi
+if [ -d "../local" ] ; then
+	mcopy -snvom -i ${img} ../local/* $TARGET/
+fi
